788c70d9addde21ec055ac75ae03203654dfa781
David Kulhánek <davidkulh182@gmail.com>
2023-05-15 14:24:10 +0200

    Update eslint config + lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,44 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', 'prettier'],
+  plugins: ['prettier'],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    yoda: [
+      'warn',
+      'always',
+      {
+        exceptRange: true,
+        onlyEquality: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
+  },
+};
